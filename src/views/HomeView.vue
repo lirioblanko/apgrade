8207e163fd0a287b62bd33f27c6ee83d07e9bdad
liliya <goldliliya@mail.ru>
2025-02-15 12:37:45 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import DatePicker from 'primevue/datepicker';
+</script>
+
+<template>
+  <main>
+    <p>ffff</p>
+    <TheWelcome />
+    <DatePicker inline showWeek class="w-full sm:w-[30rem]" />
+  </main>
+</template>
